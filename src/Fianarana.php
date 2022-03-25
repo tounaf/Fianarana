3713600938c74c354cbb46049d14c2a1dbea23e7
fetra.harinjatovo <fetra.harinjatovo@pulse.mg>
2022-03-25 15:06:27 +0300

    fianarana

---
+++ src/Fianarana.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nambinina2
+ * Date: 25/03/2022
+ * Time: 15:03
+ */
+namespace Devinbox\Fianarana;
+class Fianarana
+{
+    public function mianatra()
+    {
+        echo "HEloo workd";
+    }
+}
