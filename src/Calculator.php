0270631ca029ff357830bc3de9ef08d231d744eb
fetra.harinjatovo <fetra.harinjatovo@pulse.mg>
2022-03-25 14:20:45 +0300

    Init

---
+++ src/Calculator.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nambinina2
+ * Date: 25/03/2022
+ * Time: 14:18
+ */
+
+namespace Pulse\Calculator;
+
+
+class Calculator
+{
+    public function add($a, $b)
+    {
+        return $a + $b;
+    }
+
+    public function sub($a, $b)
+    {
+        return $a - $b;
+    }
+}
